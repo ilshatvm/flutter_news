8fc7849bb10392cac954d4156ba1a3e6760615d3
Ilshat <ilshatvm13@gmail.com>
2024-03-06 17:55:47 +0500

    added widgets

--- lib/onboarding/view/onboarding_page.dart
+++ lib/onboarding/view/onboarding_page.dart
@@ -1,13 +1,132 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news/theme/theme.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
+  static List<String> imageUrls = [
+    "assets/images/onboarding.png",
+    "assets/images/onboarding.png",
+    "assets/images/onboarding.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Onboarding Page'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CarouselWidget(imageUrls: imageUrls),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(height: 34.0),
+                  const Text(
+                    "First to know",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 24.0),
+                  const Text(
+                    "All news in one place, be the\nfirst to know last news",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.greyPrimary,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 64.0),
+                  FilledButton(
+                    onPressed: () {},
+                    child: const Text("Next"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarouselWidget extends StatefulWidget {
+  const CarouselWidget({
+    super.key,
+    required this.imageUrls,
+  });
+
+  final List<String> imageUrls;
+
+  @override
+  State<CarouselWidget> createState() => _CarouselWidgetState();
+}
+
+class _CarouselWidgetState extends State<CarouselWidget> {
+  int activeIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider.builder(
+          itemCount: widget.imageUrls.length,
+          itemBuilder: (context, index, realIndex) =>
+              CarouselItem(imageUrl: widget.imageUrls[index]),
+          options: CarouselOptions(
+            height: 336.0,
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            onPageChanged: (index, reason) {
+              setState(() {
+                activeIndex = index;
+              });
+            },
+          ),
+        ),
+        const SizedBox(height: 40.0),
+        AnimatedSmoothIndicator(
+          effect: const ExpandingDotsEffect(
+            dotHeight: 8.0,
+            dotWidth: 8.0,
+            spacing: 8.0,
+            dotColor: AppColors.greyLighter,
+          ),
+          activeIndex: activeIndex,
+          count: widget.imageUrls.length,
+        ),
+      ],
+    );
+  }
+}
+
+class CarouselItem extends StatelessWidget {
+  const CarouselItem({
+    super.key,
+    required this.imageUrl,
+  });
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.grey,
+        image: DecorationImage(
+          image: AssetImage(imageUrl),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,13 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news/theme/theme.dart';
 
 class AppTheme {
-  static ThemeData get light => ThemeData(
-        brightness: Brightness.light,
-        fontFamily: "SFPro",
-        useMaterial3: true,
-        colorScheme: _colorSchemeLight,
-      );
+  static ThemeData get light {
+    return ThemeData(
+      brightness: Brightness.light,
+      fontFamily: "SFPro",
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: AppColors.blackPrimary),
+        bodyMedium: TextStyle(color: AppColors.blackPrimary),
+        bodySmall: TextStyle(color: AppColors.blackPrimary),
+        titleLarge: TextStyle(color: AppColors.blackPrimary),
+        titleMedium: TextStyle(color: AppColors.blackPrimary),
+        titleSmall: TextStyle(color: AppColors.blackPrimary),
+        labelLarge: TextStyle(color: AppColors.blackPrimary),
+        labelMedium: TextStyle(color: AppColors.blackPrimary),
+        labelSmall: TextStyle(color: AppColors.blackPrimary),
+        displayLarge: TextStyle(color: AppColors.blackPrimary),
+        displayMedium: TextStyle(color: AppColors.blackPrimary),
+        displaySmall: TextStyle(color: AppColors.blackPrimary),
+        headlineLarge: TextStyle(color: AppColors.blackPrimary),
+        headlineMedium: TextStyle(color: AppColors.blackPrimary),
+        headlineSmall: TextStyle(color: AppColors.blackPrimary),
+      ),
+      useMaterial3: true,
+      colorScheme: _colorSchemeLight,
+      filledButtonTheme: _filledButtonThemeData,
+    );
+  }
 }
+
+const FilledButtonThemeData _filledButtonThemeData = FilledButtonThemeData(
+  style: ButtonStyle(
+    minimumSize: MaterialStatePropertyAll(Size.fromHeight(56.0)),
+    textStyle: MaterialStatePropertyAll(
+      TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    shape: MaterialStatePropertyAll(
+      RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12.0))),
+    ),
+  ),
+);
 
 const ColorScheme _colorSchemeLight = ColorScheme(
   brightness: Brightness.light,

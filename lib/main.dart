f140025f72aa184047b274ae73bf5ca80e6578b9
Ilshat <ilshatvm13@gmail.com>
2024-03-08 14:34:30 +0500

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_news/login/login.dart';
 import 'package:flutter_news/theme/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top],
+  );
   runApp(const MyApp());
 }
 

43a4792b9ac7d74e95b08f3c2718c431cc4a26f7
Ilshat <ilshatvm13@gmail.com>
2024-04-02 07:44:25 +0500

    added SliverAppBar on bookmarks_page

--- lib/bookmarks/view/bookmarks_page.dart
+++ lib/bookmarks/view/bookmarks_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/common/app_icons.dart';
 import 'package:flutter_news/common/common.dart';
 import 'package:flutter_news/home/view/home_page.dart';
 import 'package:flutter_news/theme/theme.dart';
@@ -12,22 +13,85 @@ class BookmarksPage extends StatelessWidget {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
-            const SliverPadding(
-              padding: EdgeInsets.fromLTRB(20.0, 28.0, 20.0, 32.0),
-              sliver: SliverToBoxAdapter(
-                child: AppTitle(
-                  title: "Bookmarks",
-                  description: "Saved articles to the library",
+            const SliverAppBar(
+              pinned: true,
+              snap: false,
+              floating: false,
+              expandedHeight: 88.0,
+              flexibleSpace: FlexibleSpaceBar(
+                expandedTitleScale: 1.0,
+                centerTitle: false,
+                titlePadding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 12.0),
+                title: Text(
+                  'Bookmarks',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.blackDark,
+                  ),
                 ),
               ),
             ),
+            const SliverPadding(
+              padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 32.0),
+              sliver: SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 20,
+                  child: Text(
+                    'Saved articles to the library',
+                    style: TextStyle(
+                      color: AppColors.greyPrimary,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // const SliverPadding(
+            //   padding: EdgeInsets.fromLTRB(20.0, 28.0, 20.0, 32.0),
+            //   sliver: SliverToBoxAdapter(
+            //     child: AppTitle(
+            //       title: "Bookmarks",
+            //       description: "Saved articles to the library",
+            //     ),
+            //   ),
+            // ),
             SliverPadding(
               padding: const EdgeInsets.all(0.0),
-              sliver: SliverList.builder(
-                itemCount: newsList.length,
-                itemBuilder: (context, index) =>
-                    NewsItemWidgetSmall(news: newsList[index]),
-              ),
+              sliver: newsList.isEmpty
+                  ? const SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: AppColors.greyLighter,
+                            radius: 36.0,
+                            child: Icon(
+                              AppIcons.book,
+                              color: AppColors.purplePrimary,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(60.0, 24.0, 60.0, 0.0),
+                            child: Text(
+                              "You haven't saved any articles\nto yet. Start reading and\nbookmarking them now",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: AppColors.blackPrimary,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : SliverList.builder(
+                      itemCount: newsList.length,
+                      itemBuilder: (context, index) =>
+                          NewsItemWidgetSmall(news: newsList[index]),
+                    ),
             ),
           ],
         ),
@@ -92,52 +156,25 @@ class BookmarksPage extends StatelessWidget {
   }
 }
 
-// class News {
-//   final String imageUrl;
-//   final String tag;
-//   final String title;
-
-//   const News({
-//     required this.imageUrl,
-//     required this.tag,
-//     required this.title,
-//   });
-// }
-
-// List<String> categories = [
-//   "Sports",
-//   "Gaming",
-//   "Politics",
-//   "Life",
-//   "Animals",
-//   "Nature",
-//   "Food",
-//   "Art",
-//   "History",
-//   "Fashion",
-//   "Covid-19",
-//   "Middle East",
-// ];
-
 List<News> newsList = [
-  const News(
-    imageUrl: "assets/images/news_1.png",
-    tag: "Politics",
-    title: "The latest situation in the presidential election",
-  ),
-  const News(
-    imageUrl: "assets/images/news_2.png",
-    tag: "Art",
-    title: "An updated daily front page",
-  ),
-  const News(
-    imageUrl: "assets/images/news_1.png",
-    tag: "Politics",
-    title: "The latest situation in the presidential election",
-  ),
-  const News(
-    imageUrl: "assets/images/news_2.png",
-    tag: "Art",
-    title: "An updated daily front page",
-  ),
+  // const News(
+  //   imageUrl: "assets/images/news_1.png",
+  //   tag: "Politics",
+  //   title: "The latest situation in the presidential election",
+  // ),
+  // const News(
+  //   imageUrl: "assets/images/news_2.png",
+  //   tag: "Art",
+  //   title: "An updated daily front page",
+  // ),
+  // const News(
+  //   imageUrl: "assets/images/news_1.png",
+  //   tag: "Politics",
+  //   title: "The latest situation in the presidential election",
+  // ),
+  // const News(
+  //   imageUrl: "assets/images/news_2.png",
+  //   tag: "Art",
+  //   title: "An updated daily front page",
+  // ),
 ];
